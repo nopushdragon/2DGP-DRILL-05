6b0f78790e405f0df66ffbf7ec1914cf925583bc
AN MIN YONG <iks05280204@gmail.com>
2025-09-25 19:43:48 +0900

    키 입력받고 캐릭터 띄울 코드 뼈대 생성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,11 +4,20 @@ open_canvas(800,600)
 background = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def handle_events():
+    pass
+
+x = 400
+y = 300
+frame = 0
+
 
 while True:
     clear_canvas()
     background.clip_draw(0,0,1280,1024,400,300,800,600)
+    #캐릭터 그리기
     update_canvas()
+    frame = (frame + 1) % 8
     delay(0.05)
     pass
 
